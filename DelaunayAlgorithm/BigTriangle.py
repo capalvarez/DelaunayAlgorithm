f753f263fba6e22e378b1d27c2702c8e8b03fc54
capalvarez <capalvarez.i@gmail.com>
2015-03-18 13:48:06 -0300

    Inicio de tarea

---
+++ DelaunayAlgorithm/BigTriangle.py
@@ -0,0 +1,21 @@
+__author__ = 'cata'
+
+def bigTriangle():
+    #Encargado de devolver los tres puntos que forman el triangulo inicial del algoritmo
+
+
+
+
+def findCorners(pointList):
+    #Encuentra las coordenadas mas a la izq, der, arriba y abajo (para encontrar el cuadrado cobertor de los puntos)
+    up = pointList[0]
+    down = pointList[0]
+    right = pointList[0]
+    left = pointList[0]
+
+    for i in range(len(pointList)):
+        
+
+
+
+
